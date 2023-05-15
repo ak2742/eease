130ae796aa7b998ca432a4038194f8dbc5f0e356
AK <88041902+ak2742@users.noreply.github.com>
2023-05-15 05:55:17 +0530

    255846

---
+++ index.js
@@ -0,0 +1,24 @@
+const dbConnect = require("./dbConnect");
+const express = require("express");
+const path = require("path");
+const app = express();
+require('dotenv').config()
+const port = process.env.EXPRESS_PORT || 80
+
+dbConnect();
+
+app.use(require('cors')());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/auth", require("./apis/user"));
+app.use("/api/notes", require("./apis/notes"));
+
+app.use("/app", express.static(path.join(__dirname, 'build')))
+app.get('/app/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
