856f1a4e7eab2dafaaa035323a40c04d68591b29
AK <88041902+ak2742@users.noreply.github.com>
2023-05-19 05:56:22 +0530

    15zzmmz

---
+++ src/components/NewsItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function NewsItem(props) {
+    let { source, title, body, author, date, newsUrl, imgUrl } = props
+
+    const defaultImg = `https://${process.env.REACT_APP_DEFAULT_IMG}`
+
+    let myStyle = {
+        color: props.mode === 'dark' ? '#ffffff' : '#000000',
+        backgroundColor: props.mode === 'dark' ? '#071f42' : '#ffffff',
+    }
+
+    return (
+        <div className="card mb-3" style={myStyle}>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                position: 'absolute',
+                right: '0'
+            }}
+            >
+                <span className="badge rounded-pill bg-danger">{source}</span>
+            </div>
+            <img src={!imgUrl ? defaultImg : imgUrl} className="card-img-top" alt="could not load this content" />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{body}</p>
+                <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                <a rel="noreferrer nofollow" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+            </div>
+        </div>
+    )
+}
+
+export default NewsItem
